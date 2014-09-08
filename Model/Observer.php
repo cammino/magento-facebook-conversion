34d6f6d0770f343bce26cb95b85015d2c76408e1
Lucas Guarnieri <guarnieri@cammino.com.br>
2014-09-08 16:53:41 -0300

    Adapta modulo de adwords para facebook

--- Model/Observer.php
+++ Model/Observer.php
@@ -1,9 +1,17 @@
 <?php
-class Cammino_Adwords_Model_Observer
+class Cammino_Facebookconversion_Model_Observer
 {
 	public function addTracker(Varien_Event_Observer $observer) {
-		$block = Mage::app()->getFrontController()->getAction()->getLayout()->createBlock("adwords/tracker");
-		Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('content')->append($block);
+
+		
+		$block = Mage::app()->getFrontController()->getAction()->getLayout()->createBlock("facebookconversion/tracker");
+
+		$blockContent = Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('content');
+
+		if ($blockContent) {
+			$blockContent->append($block);
+		}
+
 	}
 }
 ?>
